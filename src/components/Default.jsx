aad75c6ce633ff931a679f56ed4f7b7c7f73f1d1
Hazrat Mosaddique Ali <azm.code@gmail.com>
2025-02-09 18:28:16 +0000

    Promise Done

--- src/components/Default.jsx
+++ src/components/Default.jsx
@@ -1,5 +1,45 @@
+import { useState } from "react";
+
 const Default = () => {
-  return <h1>Hello World!</h1>;
+  const initialMovies = [
+    {
+      id: 1,
+      title: "Inception",
+      year: 2010,
+      rating: 8.8,
+    },
+    {
+      id: 2,
+      title: "The Dark Knight",
+      genre: "Action",
+      rating: 9.0,
+    },
+  ];
+
+  const [movies, setMovies] = useState(initialMovies);
+
+  const handleClick = () =>
+    setMovies(
+      movies.map((movie) =>
+        movie.id === 1 ? { ...movie, rating: "Bromance" } : movie
+      )
+    );
+  return (
+    <div>
+      {movies.map((movie, index) => (
+        <p className="font-playfair" key={index}>
+          {movie.title}
+          <span className="ml-4 opacity-50 hover:opacity-70">
+            {movie.rating}
+          </span>
+        </p>
+      ))}
+
+      <button className="p-1 my-2 rounded-md font-lato" onClick={handleClick}>
+        Change Name
+      </button>
+    </div>
+  );
 };
 
 export default Default;
